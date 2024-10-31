16b70030cbc548b80406dc35e685b45c9ff8f29a
Russell Stansfield <120559768+russell-stansfield@users.noreply.github.com>
2024-10-31 14:59:04 +0100

    Added Salted Hash

---
+++ backend/DeerDiary_Backend/Security/salty-hash.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DeerDiary_Backend.Security
+{
+    public class PasswordHasher
+    {
+        // Hash Password and generate and store Salt.
+        public static string HashPassword(string password, out string salt)
+        {
+            byte[] saltBytes = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            salt = Convert.ToBase64String(saltBytes);
+
+            string saltedPassword = salt + password;
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
+
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        // Hash Password with existing salt
+        public static string HashPasswordWithSalt(string password, string salt)
+        {
+            string saltedPassword = salt + password;
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
+
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public static bool VerifyPassword(string password, string storedHash, string storedSalt)
+        {
+            string hashOfInput = HashPasswordWithSalt(password, storedSalt);
+
+            return hashOfInput.Equals(storedHash, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
